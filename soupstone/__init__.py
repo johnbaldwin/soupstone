ecd26412fd9f9b5d9151423eacaa861f9534a2a1
John Baldwin <jlbaldwin@gmail.com>
2014-04-01 00:58:32 -0400

    added init

---
+++ soupstone/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Souptone is a catch-all Python library of tidbits of functionality I don't want to keep repeating (DRY princile) and which I don't *yet* have a better home.
+
+"""
+
+__title__ = 'soupstone'
+__version__ = '0.0.1'
+__author__ = 'John Baldwin'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2014 John Baldwin'
+
+
